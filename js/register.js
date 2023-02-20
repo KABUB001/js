745df2ac6c021737a5243e83655a49374b9f8bc0
KABUB001 <berangerkoffi98@gmail.com>
2023-02-20 16:01:25 +0000

    update file

--- js/register.js
+++ js/register.js
@@ -1,9 +1,35 @@
-var nm = document.getElementById("name");
-var mail = document.getElementById("mail");
-var mdp = document.getElementById("password");
-var rmdp = document.getElementById("rpassword");
+var username = document.getElementById("name").value;
+var mail = document.getElementById("email").value;
+var mdp = document.getElementById("password").value;
+var rmdp = document.getElementById("rpassword").value;
+var result = document.getElementById("result");
+console.log(mail)
 
-nm.addEventListener("input", function(e){
+function signUp(e) {
+    e.preventDefault();
+
+
+
+/*var user = {
+    email: mail,
+    username: username,
+    password: mdp,
+    rpassword: rmdp
+};
+console.log(user)
+
+    let json = JSON.stringify(user);
+localStorage.setItem(username, json);
+console.log("user added");
+
+*/
+}
+
+
+
+
+
+/*nm.addEventListener("input", function(e){
     localStorage.setItem('name', e.target.value)
 })
 mail.addEventListener("input", function(e){
@@ -14,4 +40,18 @@ mdp.addEventListener("input", function(e){
 })
 rmdp.addEventListener("input", function(e){
     localStorage.setItem('rpassword', e.target.value)
-})
+})*/
+
+/*function loginFunc(e){
+    event.preventDefault();
+var username = document.getElementById("name").value;
+var mdp = document.getElementById("password").value
+var result = document.getElementById("result");
+var user = localStorage.getItem(username);
+var data = JSON.parse(user);
+console.log(data)
+    
+if(user == null){
+    result.innerHTML =
+}
+}*/
